3d48b441969f9ae39a742074be7b7314caeb6a32
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2023-04-06 16:49:38 +0200

    MODIF

--- base/src/lgacdb02.cbl
+++ base/src/lgacdb02.cbl
@@ -1,7 +1,7 @@
        PROCESS SQL
       ******************************************************************
       *                                                                *
-      * (C) Copyright IBM Corp. 2011, 2022                             *
+      * (C) Copyright IBM Corp. 2011, 2023                             *
       *                                                                *
       *                    ADD Customer                                *
       *                                                                *
