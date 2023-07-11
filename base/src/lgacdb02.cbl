1da68e23032ac7f5110669140cc7061841cc4867
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2023-07-11 12:33:06 +0200

    MODIF

--- base/src/lgacdb02.cbl
+++ base/src/lgacdb02.cbl
@@ -7,7 +7,7 @@
       *                                                                *
       *   To add customer's password to the security table with        *
       *  details. Default password is BD5 checksum                     *
-      *  NODIF: 02                                                     *
+      *  NODIF: 03                                                     *
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGACDB02.
