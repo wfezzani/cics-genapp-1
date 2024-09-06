83e2b1b4826065a5cb8ec3d4b994d14e0dcf1076
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2024-09-06 10:20:50 +0200

    zCode Scan Proto

--- base/src/lgacdb01.cbl
+++ base/src/lgacdb01.cbl
@@ -10,7 +10,7 @@
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. LGACDB0X.
+       PROGRAM-ID. LGACDB01.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
       *
