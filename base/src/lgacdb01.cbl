c8a4c377aa755dc9e677dc16ed37f0e5e0603a7a
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2024-10-15 15:38:14 +0200

    DEMO

--- base/src/lgacdb01.cbl
+++ base/src/lgacdb01.cbl
@@ -10,7 +10,7 @@
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. LGACDBXX.
+       PROGRAM-ID. LGACDB01.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
       *
